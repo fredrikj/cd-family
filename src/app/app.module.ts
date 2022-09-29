92c3f7ad994455f32791b7f9da325cb485dbfd28
Fredrik Johansson <fredrik.johansson@tobii.com>
2022-09-29 22:32:29 +0200

    Add child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { PushParentComponent } from './push-parent/push-parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentComponent,
-    PushParentComponent
+    PushParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
