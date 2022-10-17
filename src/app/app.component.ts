0dbfdc4d1bd73b9bcb786eb30183c3c169511087
Fredrik Johansson <fredrik.johansson@tobii.com>
2022-10-17 21:55:19 +0200

    Add some root check time info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   counter = 0;
+  docheckTime = 0;
+  viewcheckcounter = 0;
   get name() {
     console.log(`root CD ${(this.counter += 1)}`);
     return 'root';
+  }
+  ngDoCheck() {
+    this.docheckTime = performance.now();
+    console.log(`root docheck`);
+  }
+  ngAfterViewChecked() {
+    console.log(
+      `root viewchecked ${(this.viewcheckcounter += 1)} ${(
+        performance.now() - this.docheckTime
+      ).toFixed(1)}`
+    );
   }
 }
